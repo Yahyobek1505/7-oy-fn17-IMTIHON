c5929798ac4ff599380355270eeb89c50f9031e8
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-05-03 18:32:58 +0500

    exam is start

---
+++ src/App.jsx
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Nav from "./components/Nav";
+import Home from "./pages/home/Home";
+import About from "./components/About";
+import Footer from "./components/Footer";
+// pages
+import Headphones from "./pages/Headphones";
+import Speakers from "./pages/Speakers";
+import Earphones from "./pages/Earphones";
+import ProductDetails from "./pages/ProductDetails";
+import Checkout from "./pages/Checkout";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+
+function App() {
+  const [cartItemCount, setCartItemCount] = useState(0);
+
+  const ProtectedRoute = ({ children }) => {
+    const user = localStorage.getItem("user");
+    return user ? children : <Navigate to="/login" replace />;
+  };
+
+  return (
+    <div className="App">
+      <Nav cartItemCount={cartItemCount} setCartItemCount={setCartItemCount} />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/headphones"
+          element={
+            <ProtectedRoute>
+              <Headphones />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/speakers"
+          element={
+            <ProtectedRoute>
+              <Speakers />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/earphones"
+          element={
+            <ProtectedRoute>
+              <Earphones />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/products/:slug"
+          element={<ProductDetails setCartItemCount={setCartItemCount} />}
+        />
+        <Route
+          path="/checkout"
+          element={
+            <ProtectedRoute>
+              <Checkout />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,110 @@
+import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+function LoginPage() {
+  const navigate = useNavigate();
+
+  const [errors, setErrors] = useState({});
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    const email = emailRef.current.value.trim();
+    if (!email) {
+      newErrors.email = "Email address is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      newErrors.email = "Please enter a valid email address";
+      isValid = false;
+    }
+
+    const password = passwordRef.current.value.trim();
+    if (!password) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const isLoggedIn = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    return storedUser && storedUser.email && storedUser.password;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validateForm()) {
+      const email = emailRef.current.value.trim();
+      const password = passwordRef.current.value.trim();
+
+      if (isLoggedIn(email, password)) {
+        navigate("/");
+      } else {
+        setErrors({ general: "Invalid email or password" });
+      }
+    } else {
+      alert("Form validation failed!");
+    }
+  };
+
+  return (
+    <div className="flex items-center flex-col pb-20 mt-20">
+      <div className="w-[400px] p-8 bg-[#161D2F] rounded-[20px]">
+        <h2 className="text-4xl text-white mb-10">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Email address"
+            ref={emailRef}
+            className={`border-b-2 pl-4 pb-5 outline-none w-full bg-transparent text-white mb-6 border-[#5A698F] ${
+              errors.email ? "border-red-500" : ""
+            }`}
+          />
+          {errors.email && (
+            <p className="text-red-500 text-sm mb-2">{errors.email}</p>
+          )}
+
+          <input
+            type="password"
+            placeholder="Password"
+            ref={passwordRef}
+            className={`border-b-2 pl-4 pb-5 outline-none w-full bg-transparent text-[#d87d4a] mb-6 border-[#5A698F] ${
+              errors.password ? "border-red-500" : ""
+            }`}
+          />
+          {errors.password && (
+            <p className="text-red-500 text-sm mb-2">{errors.password}</p>
+          )}
+          {errors.general && (
+            <p className="text-red-500 text-sm mb-2">{errors.general}</p>
+          )}
+
+          <button
+            type="submit"
+            className="rounded-lg py-4 px-24 text-sm text-white bg-[#d87d4a] hover:text-[#e09b73] mb-6 w-full"
+          >
+            Login
+          </button>
+        </form>
+
+        <p className="text-center flex gap-2 justify-center text-white text-[15px]">
+          Do not have an account?{" "}
+          <span
+            className="text-[#d87d4a] hover:text-[#d87d4a] duration-300 cursor-pointer"
+            onClick={() => navigate("/register")}
+          >
+            Register
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
